2da25b1e6ab65881d7e07f8595a8cd4c4c070ced
Raymond Kroon <r.kroon@energyart.nl>
2013-03-15 08:47:51 +0100

    er zijn nu twee packages...

--- nuget-publish-sms-servicestack.ps1
+++ nuget-publish-sms-servicestack.ps1
@@ -1,5 +1,5 @@
 Remove-Item SMS.ServiceStack.*.nupkg
 
 .\.nuget\NuGet.exe pack -Symbols .\SMS.ServiceStack\SMS.ServiceStack.csproj -Prop Configuration=Release -Build
-$package = (Get-ChildItem * -include *.nupkg).Name;
+$package = (Get-ChildItem * -include *.nupkg | Sort-Object Name)[0].Name;
 .\.nuget\NuGet.exe push $package $env:NuGetApiKey
