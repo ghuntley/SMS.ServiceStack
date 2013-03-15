ec3f7f2ed1a29e0f6160a63c0388ba351d882811
Raymond Kroon <r.kroon@energyart.nl>
2013-03-15 08:40:29 +0100

    adds source to symbol source

--- nuget-publish-sms-servicestack.ps1
+++ nuget-publish-sms-servicestack.ps1
@@ -1,5 +1,5 @@
 Remove-Item SMS.ServiceStack.*.nupkg
 
-.\.nuget\NuGet.exe pack .\SMS.ServiceStack\SMS.ServiceStack.csproj -Prop Configuration=Release -Build
+.\.nuget\NuGet.exe pack -Symbols .\SMS.ServiceStack\SMS.ServiceStack.csproj -Prop Configuration=Release -Build
 $package = (Get-ChildItem * -include *.nupkg).Name;
 .\.nuget\NuGet.exe push $package $env:NuGetApiKey
